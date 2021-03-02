17b2a11c8e34b5d7e917f6e006f2e59246e9b039
Jake Holland <hollandjake@hotmail.co.uk>
2021-03-02 13:10:34 +0000

    Improving the about us page (#27)

--- about/index.php
+++ about/index.php
@@ -10,7 +10,7 @@
     </style>
     <div class="text-white text-center m-auto">
         <h1 class="text-center mb-5">About</h1>
-        <p class="lead">Produced as a result of my research undertaken as part of the COM3610 dissertation module at the
-            University of Sheffield (2020&nbsp;-&nbsp;2021)</p>
+        <p class="lead">Produced as a result of my research undertaken as part of the COM3610 dissertation module at University of Sheffield<br>(2020&nbsp;-&nbsp;2021)</p>
+        <a class="lead text-white" href="https://github.com/hollandjake/pda-cfg-generator">GitHub Project</a>
     </div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/footer.php'; ?>
